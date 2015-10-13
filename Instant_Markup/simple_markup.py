63e2565fba8c553beeca3b01bdcb56fe9000ae1e
coffeephantom <coffeephantom@users.noreply.github.com>
2015-10-13 17:37:47 +0800

    生成html文件模块

---
+++ Instant_Markup/simple_markup.py
@@ -0,0 +1,20 @@
+import sys, re
+__author__ = 'Administrator'
+
+from util import *
+
+print '<html><head><title>...</title><body'
+
+title = True
+for block_element in blocks(sys.stdin):
+    block_element = re.sub(r'\*(.+?)\*', r'<em>\l</em>', block_element)
+    if title:
+        print '<h1>'
+        print block_element
+        print '</h1>'
+        title = False
+    else:
+        print '<p>'
+        print block_element
+        print '</p>'
+    print '</body></html>'
