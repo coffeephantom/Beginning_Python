221cb94ca16bd2d9f55d2f064414e344da1547f4
coffeephantom <coffeephantom@users.noreply.github.com>
2015-10-14 16:12:10 +0800

    修改错误

--- Instant_Markup/simple_markup.py
+++ Instant_Markup/simple_markup.py
@@ -8,7 +8,7 @@ def markup(file):
     new_content = ['<html><head><title>...</title><body>']
     title = True
     for block_element in block(file):
-        block_element = re.sub(r'\*(.+?)\*', r'<em>\l</em>', block_element)
+        block_element = re.sub(r'\*(.+?)\*', r'<em>\1</em>', block_element)
         if title:
             new_content.append('<h1>')
             new_content.append(block_element)
