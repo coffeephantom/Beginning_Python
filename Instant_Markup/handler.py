656d27a3f63e8bae0b9a770cd1f054d0f32623e4
coffeephantom <jfyy00@126.com>
2015-10-15 21:12:23 +0800

    添加html渲染累，并添加添加标准html标签方法

---
+++ Instant_Markup/handler.py
@@ -0,0 +1,16 @@
+#-*-coding:utf-8-*-
+
+import re
+__author__ = 'coffeephantom'
+
+class HTMLParser:
+    def start_doc(self):
+        print '<html><head><title>...</title></head><body>'
+
+
+    def end_doc(self):
+        print '</body></html>'
+
+
+
+
