4437d4b160863e5591e7ac0741c3aa1b9bbdcd20
coffeephantom <jfyy00@126.com>
2015-10-17 19:57:03 +0800

    添加段落相关标签

--- Instant_Markup/handler.py
+++ Instant_Markup/handler.py
@@ -1,28 +1,31 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 import re
+
 __author__ = 'coffeephantom'
+
 
 class HTMLParser:
     def start_doc(self):
         print '<html><head><title>...</title></head><body>'
 
-
     def end_doc(self):
         print '</body></html>'
-
 
     def start_title(self):
         print '<h1>'
 
-
     def end_title(self):
         print '</h1>'
-
 
     def start_em(self):
         print '<em>'
 
-
     def end_em(self):
         print '</em>'
+
+    def start_para(self):
+        print '<p>'
+
+    def end_para(self):
+        print '</p>'
