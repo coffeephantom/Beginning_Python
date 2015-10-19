441e93bb06dc3ef94b40848382d6a7a7fe85f62e
coffeephantom <jfyy00@126.com>
2015-10-20 00:27:06 +0800

    添加ListRule类

--- Instant_Markup/Rule.py
+++ Instant_Markup/Rule.py
@@ -31,3 +31,19 @@ class ListItemRule(Rule):
         handler.start(self, type)
         handler.feed(block[1:].strip())
         handler.end(self, type)
+
+
+class ListRule(ListItemRule):
+    type = 'list'
+    inside = False
+    def condition(self, block):
+        return True
+
+    def action(self, block, handler):
+        if not self.inside and ListItemRule.condition(self, block):
+            handler.start(self, type)
+            self.inside = True
+        elif self.inside and not ListItemRule.condition(self, block):
+            handler.end(self, type)
+            self.inside = False
+        return False
