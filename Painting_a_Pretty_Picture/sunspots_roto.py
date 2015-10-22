dc352a93c96c166a42b75143598aaed26e480466
coffeephantom <coffeephantom@users.noreply.github.com>
2015-10-22 14:45:59 +0800

    按照教程中实现绘图程序 -读取数据 -优化绘制图像

--- Painting_a_Pretty_Picture/sunspots_roto.py
+++ Painting_a_Pretty_Picture/sunspots_roto.py
@@ -1,26 +1,41 @@
 from reportlab.lib import colors
 from reportlab.graphics.shapes import *
+from reportlab.graphics.charts.lineplots import LinePlot
+from reportlab.graphics.charts.textlabels import Label
 from reportlab.graphics import renderPDF
+import urllib2
+import urllib
 
-data = [
-    (2015, 4, 127.6, 128.6, 126.6),
-    (2015, 5, 123.9, 124.9, 122.9),
-    (2015, 6, 119.8, 121.8, 117.8),
-    (2015, 7, 116.3, 119.3, 113.3),
-    (2015, 8, 113.8, 117.8, 109.8),
-    (2015, 9, 111.8, 115.8, 107.8)
-]
 
-drawing = Drawing(200, 150)
+URL = 'http://services.swpc.noaa.gov/text/predicted-sunspot-radio-flux.txt'
+COMMENT_CHARS = '#:'
 
-pred = [row[2] - 40 for row in data]
-high = [row[3] - 40 for row in data]
-low = [row[4] - 40 for row in data]
-times = [200 * ((row[0] + row[1] / 12.0) - 2015)  for row in data]
 
-drawing.add(PolyLine(zip(times, pred), strokeColor=colors.blue))
-drawing.add(PolyLine(zip(times, high), strokeColor=colors.red))
-drawing.add(PolyLine(zip(times, low), strokeColor=colors.green))
-drawing.add(String(65, 115, 'Sunspots', fontSize=18, fillColor=colors.red))
+drawing = Drawing(400,200)
+
+data = []
+
+for line in urllib2.urlopen(URL).readlines():
+    if not line.isspace() and not line[0] in COMMENT_CHARS:
+        data.append([float(n) for n in line.split()])
+pred = [row[2] for row in data]
+high = [row[3] for row in data]
+low = [row[4] for row in data]
+times = [row[0] + row [1]/12.0 for row in data]
+
+lp = LinePlot()
+lp.x = 50
+lp.y = 50
+
+lp.height = 125
+lp.width = 300
+
+lp.data = [zip(times, pred),zip(times, high), zip(times, low)]
+lp.lines[0].strokeColor = colors.blue
+lp.lines[1].strokeColor = colors.red
+lp.lines[2].strokeColor = colors.green
+
+drawing.add(lp)
+drawing.add(String(250, 150, 'Sunspots', fontSize=14, fillColor=colors.red))
 
 renderPDF.drawToFile(drawing, 'report1.pdf', 'Sunspots')
