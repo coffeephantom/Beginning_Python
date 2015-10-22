1564bbbbe88395c4f3458f029eb9de2b668ddc37
coffeephantom <jfyy00@126.com>
2015-10-22 23:20:06 +0800

    添加HealineHandler类

--- XML_for_All_Occasions/demo.py
+++ XML_for_All_Occasions/demo.py
@@ -8,5 +8,29 @@ class TestHandler(ContentHandler):
     def startElement(self, name, attrs):
         print name, attrs.keys()
 
+
+class HeadlineHandler(ContentHandler):
+    in_headline = False
+    def __init__(self, headlines):
+        ContentHandler.__init__()
+        self.headlines = headlines
+        self.data = []
+
+    def startElement(self, name, attrs):
+        if name == 'h1':
+            self.in_headline = True
+
+    def endElement(self, name):
+        if name == 'h1':
+            text = ''.join(self.data)
+            self.data = []
+            self.headlines.append(text)
+            self.in_headline = False
+
+    def characters(self, content):
+        if self.in_headline:
+            self.data.append(content)
+
+
 if __name__ == '__main__':
     parse('website.xml', TestHandler())
