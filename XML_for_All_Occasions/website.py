e2a81c710cefc5dd4ce7f85706b8f1b03b69ccaa
coffeephantom <jfyy00@126.com>
2015-10-26 22:53:31 +0800

    添加教程中历程，完成Dispatcher(调度－控制如何调用方法)类输入

---
+++ XML_for_All_Occasions/website.py
@@ -0,0 +1,28 @@
+# -*-coding:utf-8-*-
+__author__ = 'coffeephantom'
+
+from xml.sax.handler import ContentHandler
+from xml.sax import parse
+import os
+
+class Dispatcher:
+    def dispatch(self, prefix, name, attrs=None):
+        mname = prefix + name.capitalized()
+        dname = 'default' + prefix.capitalized()
+        method = getattr(self, mname, None)
+        if callable(method):
+            args = ()
+        else:
+            method = getattr(self, dname, None)
+            args = name
+        if prefix == 'start':
+            args += attrs
+        if callable(method):
+            method(*args)
+
+    def startElement(self, name, attrs):
+        self.dispatch('start', name, attrs)
+
+    def endElement(self, name):
+        self.dispatch('end', name)
+
